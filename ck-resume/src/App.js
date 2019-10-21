ff0cb8ad77a9d451b92577da52ab476254fc21c6
cedrickevers <keverscedric@gmail.com>
2019-10-21 11:31:44 +0200

    set nav with react-mdl

--- ck-resume/src/App.js
+++ ck-resume/src/App.js
@@ -1,12 +1,33 @@
-import React, {Component} from 'react';
+ 
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="demo-big-content">
-        <h1>Test</h1>
-      </div>
+<div style={{height: '300px', position: 'relative'}}>
+    <Layout fixedHeader>
+        <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title="Title">
+            <Navigation>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+                <a href="#">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+    </Layout>
+</div>
 
     );
   }
