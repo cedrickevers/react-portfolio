3500c44581f3ef1408d406baa3b0fde6f3abbd65
cedrickevers <keverscedric@gmail.com>
2019-10-21 16:38:53 +0200

    socials-networks and styles on landing pag

--- ck-resume/src/components/landingpage.js
+++ ck-resume/src/components/landingpage.js
@@ -13,6 +13,32 @@ class Landing extends Component {
                          
                         <div className="banner-text">
                             <h1>Full troll web dev</h1>
+                            <hr/>
+                            <p> HTML / CSS | Javasript/ NodeJS / React | PHP / Laravel / SEO </p>
+
+                            <div className="social-links">
+
+{/* LinkedIn */}
+<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+  <i className="fa fa-linkedin-square" aria-hidden="true" />
+</a>
+
+{/* Github */}
+<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+  <i className="fa fa-github-square" aria-hidden="true" />
+</a>
+
+{/* Freecodecamp */}
+<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+  <i className="fa fa-free-code-camp" aria-hidden="true" />
+</a>
+
+{/* Youtube */}
+<a href="http://google.com" rel="noopener noreferrer" target="_blank">
+  <i className="fa fa-youtube-square" aria-hidden="true" />
+</a>
+
+</div>
                         </div> 
                                    </Cell>
                 </Grid>
--- ck-resume/src/components/projects.js
+++ ck-resume/src/components/projects.js
@@ -1,9 +1,27 @@
 import React, {Component} from "react";
 
 class Projects extends Component {
-    render () {
-        return (
-            <div><h1>test pages</h1></div>
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 2 };
+    }
+
+            
+                render() {
+                    return (
+                        <div className="demo-tabs">
+                            <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                                <Tab>Starks</Tab>
+                                <Tab>Lannisters</Tab>
+                                <Tab>Targaryens</Tab>
+                            </Tabs>
+                            <section>
+                                <div className="content">Content for the tab: {this.state.activeTab}</div>
+                            </section>
+                        </div>    
+                    );
+                }
+            }
         )
     }
 }
