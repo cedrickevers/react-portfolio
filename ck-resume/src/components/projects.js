34cbd446bf65d00b9aea8f871b7742a0be3aff28
cedrickevers <keverscedric@gmail.com>
2019-10-22 11:25:58 +0200

    add tab result

--- ck-resume/src/components/projects.js
+++ ck-resume/src/components/projects.js
@@ -1,25 +1,72 @@
-import React, {Component} from "react";
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
-
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardText,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton
+} from "react-mdl";
 
 class Projects extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { activeTab: 2 };
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 2 };
+  }
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <h1>this is reacgt</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1> this is not react</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1> this is not react</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1> this is not react</h1>
+        </div>
+      );
     }
+  }
 
-    render () {
-        return (
-            <div>
-            <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-              <Tab>React</Tab>
-              <Tab>NodeJS</Tab>
-              <Tab>Laravel</Tab>
-              <Tab>Css</Tab>
-            </Tabs>
-    
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>NodeJS</Tab>
+          <Tab>Laravel</Tab>
+          <Tab>Css</Tab>
+        </Tabs>
 
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
 export default Projects;
