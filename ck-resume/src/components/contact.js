175b4a6aad9d5d4e180610177a7f8c494d54dbbd
cedrickevers <keverscedric@gmail.com>
2019-10-22 14:11:54 +0200

    Spilit contact into two columnss

--- ck-resume/src/components/contact.js
+++ ck-resume/src/components/contact.js
@@ -1,9 +1,27 @@
 import React, {Component} from "react";
+import {
+    Tabs,
+    Tab,
+    Grid,
+    Cell,
+    Card,
+    CardTitle,
+    CardText,
+    CardActions,
+    Button,
+    CardMenu,
+    IconButton
+  } from "react-mdl";
 
 class Contact extends Component {
     render () {
         return (
-            <div><h1>test pages</h1></div>
+          <div className="contact-body">
+           <Grid className="contact-grid">
+            <Cell col={6}>col 1</Cell>
+            <Cell col={6}> Col 2</Cell>
+           </Grid>
+          </div>
         )
     }
 }
