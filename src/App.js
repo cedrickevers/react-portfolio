bcc89992263b8e968009c7454e245d883731e165
cedrickevers <keverscedric@gmail.com>
2019-10-31 14:26:01 +0100

    updating content

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ class App extends Component {
     <Layout>
         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/projects">Projects</Link>
+                <Link to="/resume">CV</Link>
+                <Link to="/projects">Projets</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
-              <Link to="/resume">Resume</Link>
-              <Link to="/projects">Projects</Link>
+              <Link to="/resume">CV</Link>
+              <Link to="/projects">Projets</Link>
               <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
