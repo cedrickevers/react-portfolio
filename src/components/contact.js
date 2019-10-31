66035860053a4eda986ccb3b85177de047ef93b3
cedrickevers <keverscedric@gmail.com>
2019-10-31 12:00:07 +0100

    content update

--- src/components/contact.js
+++ src/components/contact.js
@@ -14,7 +14,7 @@ class Contact extends Component {
               alt="avatar"
               style={{height: '250px'}}
                />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}></p>
 
           </Cell>
           <Cell col={6}>
@@ -33,7 +33,7 @@ class Contact extends Component {
                <ListItem>
                   <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
                     <i className="fa fa-envelope" aria-hidden="true"/>
-                    someone@example.com
+                    keverscedric@gmail.com
                   </ListItemContent>
                 </ListItem>
 
--- src/components/resume.js
+++ src/components/resume.js
@@ -22,18 +22,18 @@ class Resume extends Component {
             <h2 style={{paddingTop: '2em'}}>Cédric Kévers</h2>
             <h4 style={{color: 'grey'}}>Back-end</h4>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p>Préférant développer des fonctionalités que faire du visuel, c'est tout naturellement que je me suis orienté vers les technologie backend- tel que nodeJS ou laravel.<br/>J'aime avoir de l'autonomie dans mon travail et pouvoir apprendre et évoluer. </p>
+            <p>Préférant développer des fonctionalités que faire du visuel, c'est tout naturellement que je me suis orienté vers les technologie backend- tel que nodeJS ou laravel.<br/>J'aime avoir de l'autonomie dans mon travail, pouvoir apprendre et évoluer. </p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/> 
             <h5>Addresse</h5>
-            <p>09 Rue de la vallée, 4610 Bellaire</p>
+            <p>56 Rue de la vallée, 4610 Bellaire</p>
             <h5>Téléphone</h5>
             <p>(04) 74760835</p>
             <h5>Email</h5>
-            <p>k_cedric@live.fr</p>
+            <p>keverscedric@gmail.com</p>
             <h5>Web</h5>
             <p><a href="https://licorne-kawaii.Com">Mon site E-commerce</a></p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
-            <p><a href="https://sheepbay.com">Mon site E-commerce avec un partenaire</a></p>
+            <p><a href="https://sheepbay.com">Mon site E-commerce  en anglais avec un partenaire</a></p>
 
           </Cell>
           <Cell className="resume-right-col" col={8}>
@@ -41,18 +41,18 @@ class Resume extends Component {
 
 
             <Education
-              startYear={2002}
-              endYear={2006}
+              startYear={"Mai 2019"}
+              endYear={"En cours"}
               schoolName="Becode"
               schoolDescription="Formation intensive de web dévolppeur.
               Apprentissage en autonomie par des projets concrets. Cette formation m'as permit de développer mon esprit d'équipe car une grande partie des projets sont réalisés en groupe et la formation est basée sur l'entre aide entre apprenant.
-              L'accent est aussi grandement port sur la débrouillardize, car il n 'y pas de cours, les coach sont la pour nous soutenir et nous aiguiller, mais c 'est a nous de travailler et de nous appliquer à trouver nous même la solution à nos problèmes  
+              L'accent est aussi grandement port sur la capacité à se débrouiller, car il n' y pas de cours, les coach sont la pour nous soutenir et nous aiguiller, mais c'est a nous de travailler et de nous appliquer à trouver nous même la solution à nos problèmes  
               "
                />
 
                <Education
-                 startYear={2007}
-                 endYear={2009}
+                 startYear={2018}
+                 endYear={2019}
                  schoolName="Saint Laurent"
                  schoolDescription="Cour du soirs de web développeur, c'est ici que j'ai fait mes premier pas en code, parant de 0 j'ai beacoup appris sur les bonnne pratique 
                  => Respect du W3C, comment bien préparer la réalisation d'un algorithme, structure MCV, requêtes sql."
@@ -62,19 +62,14 @@ class Resume extends Component {
               <h2>Experience</h2>
 
             <Experience
-              startYear={2009}
-              endYear={2012}
-              jobName="First Job"
+              startYear={"Mai 2019"}
+              endYear={"En cours"}
+              jobName="Becode"
               jobDescription="Réalisation de travaux seul ou en équipe  avec deadline (voir la partie projet), présentation orale technique (Explication de code, SEO, Google analityc)"
               />
 
 
-              {/* <Experience
-                startYear={2012}
-                endYear={2016}
-                jobName="Second Job"
-                jobDescription="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-                /> */}
+              {}
               <hr style={{borderTop: '3px solid #e22947'}} />
               <h2>Skills</h2>
               <Skills
